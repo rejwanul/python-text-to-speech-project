1bc2f83a7283317ec2f7277fd8d780b6c1d0f8ef
Rejwanul Hoq <56622383+rejwanul@users.noreply.github.com>
2022-05-30 14:54:36 +0600

    Add files via upload

---
+++ python_text_to_speech.py
@@ -0,0 +1,9 @@
+#pip install pyttsx3 (have to install in New Terminal)
+import pyttsx3
+
+pyttsx3.speak("Hello Roni! How are you today? You are awesome boy")
+
+#Using Function
+def speak(text):
+    pyttsx3.speak(text)
+speak("Hello Rejwanul, How are you today?")
